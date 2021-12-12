7be9c8029680a4598fed907efe536323895b5a1b
weitongliao <94514476+weitongliao@users.noreply.github.com>
2021-12-12 22:15:05 +0800

    push

--- app/src/main/java/com/survivor/calendar/ListActivity.java
+++ app/src/main/java/com/survivor/calendar/ListActivity.java
@@ -3,40 +3,66 @@ package com.survivor.calendar;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.database.Cursor;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Display;
 import android.view.Gravity;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.Toast;
+
+import androidx.annotation.ColorInt;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class ListActivity extends Activity {
     private ListView lv;
+    private DBManager dbManager;
+    ArrayList<String> list = new ArrayList<>();
+    ArrayList<Event> events = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
 
+        setTitle();
+
+        list = new ArrayList<>();
+
         lv = (ListView) findViewById(R.id.lv);
         /*定义一个动态数组*/
         ArrayList<HashMap<String, Object>> listItem = new ArrayList<HashMap<String,     Object>>();
         /*在数组中存放数据*/
-        for(int i=0;i<10;i++)
-        {
+        dbManager = new DBManager(ListActivity.this);
+        dbManager.open();
+        Cursor cursor = dbManager.fetch();
+        while (cursor.moveToNext()){
+            cursor.getString(2);
             HashMap<String, Object> map = new HashMap<String, Object>();
-            map.put("ItemImage", R.drawable.a);//加入图片
-            map.put("ItemTitle", "第"+i+"行");
-            map.put("ItemText", "这是第"+i+"行");
+            list.add(cursor.getString(0));
+            String name = cursor.getString(1);
+            String title = cursor.getString(2);
+            String year = cursor.getString(3);
+            String month = cursor.getString(4);
+            String day = cursor.getString(5);
+            String hour = cursor.getString(6);
+            String minute = cursor.getString(7);
+            events.add(new Event(name, title, Integer.parseInt(year), Integer.parseInt(month),
+                    Integer.parseInt(day), Integer.parseInt(hour), Integer.parseInt(minute)));
+            map.put("ItemText", name + " " + title + " " + year + "-" + month + "-" + day + " " + hour + ":" + minute);
             listItem.add(map);
         }
+        dbManager.close();
 
         SimpleAdapter mSimpleAdapter = new SimpleAdapter(this,listItem,//需要绑定的数据
                 R.layout.item,//每一行的布局
@@ -51,7 +77,7 @@ public class ListActivity extends Activity {
             public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
 //                setTitle("你点击了第"+arg2+"行");//设置标题栏显示点击的行
 //                选中指定条目后显示弹窗
-                DiyDialog1();
+                DiyDialog1(arg2);
             }
         });
     }
@@ -60,36 +86,47 @@ public class ListActivity extends Activity {
      * 自定义1 控制普通的dialog的位置，大小，透明度
      * 在普通的dialog.show下面添加东西
      */
-    private void DiyDialog1(){
+    private void DiyDialog1(int index){
         AlertDialog.Builder alterDiaglog = new AlertDialog.Builder(ListActivity.this);
-        alterDiaglog.setIcon(R.drawable.a);//图标
-        alterDiaglog.setTitle("简单的dialog");//文字
-        alterDiaglog.setMessage("生存还是死亡");//提示消息
+//        alterDiaglog.setIcon(R.drawable.a);//图标
+        Event current = events.get(index);
+        alterDiaglog.setTitle(current.getTitle());//文字
+        String date = current.getYear() + "-" + current.getMonth() + "-" + current.getDay() + " " + current.getDay() + ":" + current.getMinute();
+        alterDiaglog.setMessage(date);//提示消息
         //积极的选择
-        alterDiaglog.setPositiveButton("生存", new DialogInterface.OnClickListener() {
+        alterDiaglog.setPositiveButton("CANCEL", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                Toast.makeText(ListActivity.this,"点击了生存",Toast.LENGTH_SHORT).show();
             }
         });
         //消极的选择
-        alterDiaglog.setNegativeButton("死亡", new DialogInterface.OnClickListener() {
+        alterDiaglog.setNegativeButton("DELETE", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                Toast.makeText(ListActivity.this,"点击了死亡",Toast.LENGTH_SHORT).show();
+                dbManager = new DBManager(ListActivity.this);
+                dbManager.open();
+                dbManager.delete(Long.parseLong(list.get(index)));
             }
         });
 
-        alterDiaglog.setNeutralButton("不生不死", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                Toast.makeText(ListActivity.this,"点击了不生不死",Toast.LENGTH_SHORT).show();
-            }
-        });
+
+
+//        alterDiaglog.setNeutralButton("不生不死", new DialogInterface.OnClickListener() {
+//            @Override
+//            public void onClick(DialogInterface dialog, int which) {
+//                Toast.makeText(ListActivity.this,"点击了不生不死",Toast.LENGTH_SHORT).show();
+//            }
+//        });
         AlertDialog dialog = alterDiaglog.create();
 
         //显示
         dialog.show();
+
+        Button positiveButton = ((AlertDialog) dialog).getButton(AlertDialog.BUTTON_POSITIVE);
+        positiveButton.setTextColor(Color.BLUE);
+        Button negativeButton = ((AlertDialog) dialog).getButton(AlertDialog.BUTTON_NEGATIVE);
+        negativeButton.setTextColor(Color.RED);
+
         //自定义的东西
         //放在show()之后，不然有些属性是没有效果的，比如height和width
         Window dialogWindow = dialog.getWindow();
@@ -97,12 +134,25 @@ public class ListActivity extends Activity {
         Display d = m.getDefaultDisplay(); // 获取屏幕宽、高用
         WindowManager.LayoutParams p = dialogWindow.getAttributes(); // 获取对话框当前的参数值
         // 设置高度和宽度
-        p.height = (int) (d.getHeight() * 0.4); // 高度设置为屏幕的0.6
-        p.width = (int) (d.getWidth() * 0.6); // 宽度设置为屏幕的0.65
+        p.height = (int) (d.getHeight() * 0.25); // 高度设置为屏幕的0.6
+        p.width = (int) (d.getWidth() * 0.8); // 宽度设置为屏幕的0.65
 
         p.gravity = Gravity.CENTER;//设置位置
 
         p.alpha = 0.8f;//设置透明度
         dialogWindow.setAttributes(p);
+    }
+
+    private void setTitle() {
+        // set title, left button, right button
+        TopView tv = findViewById(R.id.top_view_list);
+        tv.setTitle("Event List");
+        tv.setOnclickLeft(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+        tv.setRightTitle("");
     }
 }
--- app/src/main/java/com/survivor/calendar/TimeSelectActivity.java
+++ app/src/main/java/com/survivor/calendar/TimeSelectActivity.java
@@ -3,6 +3,7 @@ package com.survivor.calendar;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.TimePickerDialog;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -17,10 +18,25 @@ import com.bigkoo.pickerview.builder.TimePickerBuilder;
 import com.bigkoo.pickerview.listener.OnTimeSelectListener;
 import com.bigkoo.pickerview.view.TimePickerView;
 
+import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
+import studio.carbonylgroup.textfieldboxes.SimpleTextChangedWatcher;
+import studio.carbonylgroup.textfieldboxes.TextFieldBoxes;
+
 public class TimeSelectActivity extends Activity {
+
+    private DBManager dbManager;
+
+    private String eventText = "";
+
+    private int year;
+    private int month;
+    private int day;
+    private int hour;
+    private int minute;
 
     Date date = new Date();
     SimpleDateFormat dateFormat1= new SimpleDateFormat("yyyy-MM-dd");
@@ -31,25 +47,59 @@ public class TimeSelectActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timeselect);
 
+        setTitle();
+        setButton();
+        setText();
+
+
+    }
+
+    private String getTime(Date date) {//可根据需要自行截取数据显示
+        Log.d("getTime()", "choice date millis: " + date.getTime());
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        return format.format(date);
+    }
+
+    private void setText(){
+        final TextFieldBoxes textFieldBoxes = findViewById(R.id.text_field_boxes);
+        textFieldBoxes.setSimpleTextChangeWatcher(new SimpleTextChangedWatcher() {
+            @Override
+            public void onTextChanged(String theNewText, boolean isError) {
+                eventText = theNewText;
+            }
+        });
+    }
+
+    private void setTitle() {
+        // set title, left button, right button
+        TopView tv = findViewById(R.id.top_view);
+        tv.setTitle("Add Event");
+        tv.setOnclickLeft(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+        tv.setRightTitle("");
+    }
+
+    private void setButton() {
         final Button pick = findViewById(R.id.button);
-        pick.setText("Date:" + dateFormat1.format(date));
+        pick.setText(dateFormat1.format(date));
         pick.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                TimePickerDialog timePicker = new TimePickerDialog(TimeSelectActivity.this, AlertDialog.THEME_HOLO_LIGHT, new TimePickerDialog.OnTimeSetListener() {
-//                    @Override
-//                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-//                        pick.setText("Start Time:" + String.format("%02d", hourOfDay) + ":" + String.format("%02d", minute));
-//                        Log.e("对话框", "hourOfDay = " + hourOfDay + ", minute = " + minute);
-//                    }
-//                }, 0, 0, true);
-//                timePicker.show();
-                //时间选择器
                 TimePickerView pvTime = new TimePickerBuilder(TimeSelectActivity.this, new OnTimeSelectListener() {
                     @Override
                     public void onTimeSelect(Date date, View v) {
                         Toast.makeText(TimeSelectActivity.this, getTime(date), Toast.LENGTH_SHORT).show();
-                        pick.setText("Date:" + dateFormat1.format(date));
+                        pick.setText(dateFormat1.format(date));
+
+                        Calendar cal = Calendar.getInstance();
+                        cal.setTime(date);
+                        year = cal.get(Calendar.YEAR);
+                        month = cal.get(Calendar.MONTH) + 1;
+                        day = cal.get(Calendar.DAY_OF_MONTH);
                     }
                 })
                         .setLabel("年","月","日","时","分","秒")
@@ -60,30 +110,41 @@ public class TimeSelectActivity extends Activity {
         });
 
         final Button pick2 = findViewById(R.id.button2);
-        pick2.setText("Start Time:" + dateFormat2.format(date));
+        pick2.setText(dateFormat2.format(date));
         pick2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                    TimePickerView pvTime = new TimePickerBuilder(TimeSelectActivity.this, new OnTimeSelectListener() {
+                TimePickerView pvTime = new TimePickerBuilder(TimeSelectActivity.this, new OnTimeSelectListener() {
                     @Override
                     public void onTimeSelect(Date date, View v) {
                         Toast.makeText(TimeSelectActivity.this, getTime(date), Toast.LENGTH_SHORT).show();
-                        pick2.setText("Start Time:" + dateFormat2.format(date));
+                        pick2.setText(dateFormat2.format(date));
+
+                        Calendar cal = Calendar.getInstance();
+                        cal.setTime(date);
+                        hour = cal.get(Calendar.HOUR_OF_DAY);
+                        minute = cal.get(Calendar.MINUTE);
                     }
                 })
-                            .setLabel("年","月","日","时","分","秒")
-                            .setType(new boolean[]{false, false, false, true, true, false})
-                            .build();
+                        .setLabel("年","月","日","时","分","秒")
+                        .setType(new boolean[]{false, false, false, true, true, false})
+                        .build();
                 pvTime.show();
 
             }
         });
 
-    }
-
-    private String getTime(Date date) {//可根据需要自行截取数据显示
-        Log.d("getTime()", "choice date millis: " + date.getTime());
-        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        return format.format(date);
+        //confirm button
+        Button confirmButton = findViewById(R.id.ConfirmButton);
+        confirmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dbManager = new DBManager(TimeSelectActivity.this);
+                dbManager.open();
+                dbManager.insert(new Event("a", eventText, year, month, day, hour, minute));
+                dbManager.close();
+                finish();
+            }
+        });
     }
 }
